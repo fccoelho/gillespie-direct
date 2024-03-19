ffbffac745f3b7bce9b825ac52c49cafe95cd776
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-03-19 17:24:23 -0300

    Made gillespie.py compilable with mypyc but with much speed up.
    Started implementation in Taichi, but the language is too restrictive.

--- gillespyx/gillespie_ti.py
+++ gillespyx/gillespie_ti.py
@@ -11,32 +11,21 @@
 # -----------------------------------------------------------------------------
 __docformat__ = "restructuredtext en"
 
+from numpy.random import uniform, multinomial, exponential, random
+from numpy import arange, array, empty, zeros, log, isnan, nanmax, nan_to_num, ceil
 import numpy as np
-from numpy.random import multinomial,  random
-from numpy import arange, array, empty, zeros, log, isnan, nanmax, nan_to_num, ceil, ndarray
 import time
-from typing import List, Tuple, Callable, Union, Dict, Any
+import pdb
+import copy
+import taichi as ti
+import taichi.math as tm
 
-from multiprocessing import Pool
-
-viz: bool = False
-
-
-def dispatch(model)-> Any:
-    '''this function is necessary for paralelization'''
-    # ~ model.server = server
-    return model.GSSA()
+ti.init(arch=ti.cpu, default_ip=ti.i32, default_fp=ti.f32)
 
 
+@ti.data_oriented
 class Model:
-    pv0: np.ndarray
-    vn: List[str]
-    res: np.ndarray
-    steps: int
-    evseries: List[List[Any]]
-    tm: np.ndarray
-
-    def __init__(self, vnames: List[str], rates: List[float], inits: List[int], tmat: np.ndarray, propensity: List[Callable]) -> None:
+    def __init__(self, vnames, rates, inits, tmat, propensity):
         '''
         Class representing a Stochastic Differential equation.
         
@@ -54,21 +43,30 @@ class Model:
 
         assert tmat.dtype == 'int64'
         self.vn = vnames
-        self.rates = tuple(rates)
-        self.inits = tuple(inits)
-        self.tm = tmat
+        self.nvars = len(self.vn)
+        self.rates = ti.field(shape=(len(rates),), dtype=ti.f32)
+        self.rates.from_numpy(array(rates))
+        # self.inits = array(inits, dtype=np.int32)
+        self.inits = ti.field(shape=(self.nvars,), dtype=ti.i32)
+        self.inits.from_numpy(array(inits, dtype=np.int32))
+        self.state = ti.field(shape=(self.nvars,), dtype=ti.i32)
+        self.tm = ti.field(shape=(self.nvars, len(propensity)), dtype=ti.i32)
+        self.tm.from_numpy(tmat)
         self.pv = propensity  # [compile(eq,'errmsg','eval') for eq in propensity]
-        self.pvl: int = len(self.pv)  # length of propensity vector
-        self.pv0 = zeros(self.pvl, dtype=float)
-        self.nvars: int = len(self.inits)  # number of variables
-        self.evseries = []  # dictionary with complete time-series for each event type.
-        self.steps = 0
+        self.pvl = len(self.pv)  # length of propensity vector
+        self.pv0 = ti.field(shape=(len(self.pv),), dtype=ti.f32)
+        self.evseries = {}  # dictionary with complete time-series for each event type.
+        self.time = None
+        self.tmax = 0
+        self.series = None
+        self.steps = ti.field(dtype=ti.i32, shape=())
         self.viz = False  # if intermediate vizualization should be on
 
-    def getStats(self)-> Tuple[np.ndarray, ndarray[Any, np.dtype[Any]], int, List[List[float]]]:
+    def getStats(self):
         return self.time, self.series, self.steps, self.evseries
 
-    def run(self, method: str = 'SSA', tmax: int = 10, reps: int = 1, viz: bool = False, serial: bool = False)-> None:
+    # @ti.kernel
+    def run(self, method='SSA', tmax=10, reps=1, viz=False, serial=False):
         '''
         Runs the model.
         
@@ -82,73 +80,64 @@ class Model:
         :Return:
             a numpy array of shape (reps,tmax,nvars)
         '''
-        self.tmax: int = tmax
-        self.res = zeros((tmax, self.nvars), dtype=float)
-        tvec: np.ndarray = arange(tmax, dtype=int)
+        self.tmax = tmax
+        self.res = ti.field(shape=(tmax, self.nvars, reps), dtype=ti.f32)
+        # self.res = zeros((tmax, self.nvars, reps), dtype=np.float32)
+        trng = arange(tmax, dtype=int)
+        tvec = ti.ndarray(shape=(tmax,), dtype=ti.i32)
+        tvec.from_numpy(trng)
 
         if method == 'SSA':
-            if not serial:  # Parallel version
-                pool = Pool()
-                res = pool.map(dispatch, [self] * reps, chunksize=10)
-                self.res = array([i[0] for i in res])
-                if reps == 0:
-                    self.evseries = res[0][1]
-                else:
-                    self.evseries = [i[1] for i in res]
-                pool.close()
-                pool.join()
-            else:  # Serial
-                res = list(map(dispatch, [self] * reps))
-                self.res = array([i[0] for i in res])
-                if reps == 0:
-                    self.evseries = res[0][1]
-                else:
-                    self.evseries = [i[1] for i in res]
+            for r in range(reps):
+                res = self.GSSA(self.tmax, self.inits, self.rates, self.pv0)
+                self.res[:, :, r] = res
+
+                # if reps == 0:
+                #     self.evseries = self.res[0][1]
+                # else:
+                #     self.evseries = [i[1] for i in self.res]
 
         elif method == 'SSAct':
             pass
 
-        self.time: np.ndarray = tvec
+        self.time = tvec
         self.series = self.res
         # self.steps=steps
 
-    def GSSA(self)-> Tuple[np.ndarray, Dict[int, List[float]]]:
+    @ti.kernel
+    def GSSA(self, tmax: ti.i32, ini: ti.template(),
+             r: ti.template(), pv: ti.template()):
         '''
         Gillespie Direct algorithm
         '''
-        tmax = self.tmax
-        ini = array(self.inits)
-        # ini = copy.deepcopy(self.inits)
-        r = array(self.rates)
-        for i in r:
-            if i < 0:
-                i = 0
-        pvi = self.pv  # propensity functions
-        tm = self.tm
-        pv = self.pv0  # actual propensity values for each time step
-        tc: float = 0.0  # current time
+
+        tc: ti.f32 = 0  # current time
+        tau: ti.f32 = 0
         last_tim = 0  # first time step of results
-        evts: Dict[int,List[float]] = dict([(i, []) for i in range(len(self.pv))])
-        self.steps = 0
-        self.res[0, :] = ini
-        #        for tim in xrange(1,tmax):
+        evts = {i: [] for i in range(len(self.pv))}
+
+        self.steps[None] = 0
+        for i in range(self.res.shape[1]):
+            self.res[0, i, 0] = ini[i]
+            self.state[i] = ini[i]
+        neg: ti.i32 = -1
         while tc <= tmax:
-            i = 0
-            a0 = 0.0
-            for p in pvi:
-                pv[i] = p(r, ini)
+            i: ti.i32 = 0
+            a0: ti.f32 = 0
+            for j in ti.static(range(self.pvl)):
+                pv[i] = self.pv[j](r, ini)
                 a0 += pv[i]
                 i += 1
 
-            if pv.any():  # no change in state if pv is all zeros
-                tau = (-1 / a0) * log(random())
+            if np.sum(pv)[0] > 0:  # no change in state if pv is all zeros
+                tau = self.calc_tau(a0)
                 tc += tau
-                tim = int(ceil(tc))
+                tim = int(tm.ceil(tc))
 
-                event = multinomial(1, pv / a0)  # event which will happen on this iteration
+                e = self.sample_event(pv, a0)  # event which will happen on this iteration
 
-                e = event.nonzero()[0][0]
-                ini += tm[:, e]
+
+                ini += self.tm[:, e]
                 if tc <= tmax:
                     evts[e].append(tc)
                 # print tc, ini
@@ -164,19 +153,29 @@ class Model:
                         self.res[j, :] = self.res[last_tim, :]
                     break
                 last_tim = tim
-            # self.evseries = evts
+            self.evseries = evts
             if a0 == 0:
                 break  # breaks when no event has prob above 0
 
-        return self.res, evts
+        return self.res
+    @ti.func
+    def calc_tau(self, a0):
+        return (-1.0 / a0) * tm.log2(random())
+
+    @ti.func
+    def sample_event(self, pv, a0):
+        probs = []
+        for i in ti.static(range(self.pvl)):
+            probs.append(ti.cast(pv[i]/a0, ti.f32))
+
+        return multinomial(1, probs).nonzero()[0][0]
+
+@ti.func
+def p1(r, ini): return r[0] * ini[0] * ini[1]
 
 
-def p1(r: np.ndarray, ini: np.ndarray) -> float:
-    return r[0] * ini[0] * ini[1]
-
-
-def p2(r:np.ndarray, ini:np.ndarray) -> float:
-    return r[1] * ini[1]
+@ti.func
+def p2(r, ini): return r[1] * ini[1]
 
 
 def main():
@@ -186,8 +185,8 @@ def main():
     tm = array([[-1, 0], [1, -1], [0, 1]])
     # prop=[lambda r, ini:r[0]*ini[0]*ini[1],lambda r,ini:r[0]*ini[1]]
     M = Model(vnames=vnames, rates=rates, inits=ini, tmat=tm, propensity=[p1, p2])
-    t0: float = time.time()
-    M.run(tmax=80, reps=1000, viz=False, serial=True)
+    t0 = time.time()
+    M.run(tmax=80, reps=1000, viz=0, serial=1)
     print('total time: ', time.time() - t0)
     t, series, steps, evts = M.getStats()
     ser = series.mean(axis=0)
